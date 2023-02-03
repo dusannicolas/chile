c60d8f6d8a176ccec5150b683a6f1f46d1fc53c0
dusannicolas <dusanmatulic@gmail.com>
2023-02-03 19:26:37 -0300

    ejercicio explicacion

--- js/app.js
+++ js/app.js
@@ -1,22 +1,95 @@
-let miBoton = document.getElementById("miBoton");
+const regiones = [
+    {
+        nombre: "Región de Arica y Parinacota",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/parinacota.jpeg"
+    },
+    {
+        nombre: "Región de Tarapaca",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/parinacota.jpeg"
+    },
+    {
+        nombre: "Región de Antofagasta",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/parinacota.jpeg"
+    },
+    {
+        nombre: "Región de Atacama",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/valdivia.jpg"
+    },
+    {
+        nombre: "Región de Coquimbo",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/valdivia.jpg"
+    },
+    {
+        nombre: "Región de Valparaiso",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/valdivia.jpg"
+    },
+    {
+        nombre: "Región Metropolitana",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/valdivia.jpg"
+    },
+    {
+        nombre: "Región Libertador General Bernardo O'Higgins",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/valdivia.jpg"
+    },
+    {
+        nombre: "Región del Maule",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/valdivia.jpg"
+    },
+    {
+        nombre: "Región de Nuble",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/valdivia.jpg"
+    },
+    {
+        nombre: "Región del Biobío",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/valdivia.jpg"
+    },
+    {
+        nombre: "Región de La Araucanía",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/parinacota.jpeg"
+    },
+    {
+        nombre: "Región de Los Ríos",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/parinacota.jpeg"
+    },
+    {
+        nombre: "Región de Los Lagos",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/cementerio.jpg"
+    },
+    {
+        nombre: "Región de Aysén del General Carlos Ibáñez del Campo",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/cementerio.jpg"
+    },
+    {
+        nombre: "Región de Magallanes y de la Antártica Chilena",
+        atractivos: "Arica, Camarones, General Lagos y Putre",
+        imagen: "img/cementerio.jpg"
+    },
+];
 
+function mostrarDato(region){
+    //Buscar objeto
+    const datoregion = regiones.find(r => r.nombre === region);
+    if(!datoRegion){
+        return;
+    }
+    //Actuali<ar datos del card
+    document.getElementById('nombre-region').textContent = datoRegion.nombre;
+    document.getElementById('atractivos-turisticos').textContent = datoRegion.atractivos;
+    document.getElementById('image-region').src = datoRegion.imagen;
 
-miBoton.onclick = saludar;
-miBoton.onclick = despedir;
-
-function saludar(){
-    alert("Hola, Buenos dias");
 }
-
-function despedir(){
-    alert("Buenas Noches");
-}
-//evento addEcentListener()
-miBoton.addEventListener("click", saludar);
-miBoton.addEventListener("click", despedir);
-
-function miFuncion(){
-    alert('Haz presionado el boton con exito!!!');
-}
-
-alert("¡Bienvenidos a Cachureando!");
